728a228b38ba596159264fa6f8e5214f1d75bbb3
wanghaomin <2501204978@qq.com>
2022-07-29 15:55:34 +0800

    gForce Data Capture Demo

---
+++ GForceInputDemo/Assets/Scripts/KeyConfig/BuildConfigKey.cs
@@ -0,0 +1,18 @@
+﻿#if UNITY_EDITOR
+// Editor specific code here
+using UnityEditor;
+
+[InitializeOnLoad]
+public class GlobalConfig
+{
+    static GlobalConfig()
+    {
+        PlayerSettings.Android.keystorePass = "Oy123654";
+        PlayerSettings.Android.keyaliasName = "key";
+        PlayerSettings.Android.keyaliasPass = "Oy123654";
+    }
+}
+#endif
+
+
+
